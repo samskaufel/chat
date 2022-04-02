be218634ae612bb8cf2f7330441f38eea73649e6
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-04-02 13:11:58 -0700

    updated start screen and chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class Chat extends React.Component {
 
   render() {
     // assigns a variable name to the prop name that is being called from the Start screen
     let name = this.props.route.params.name;
+    // assigns a variable name to the prop chatBg that is being called from the Start screen
+    let chatBg = this.props.route.params.chatBg;
     // the user's name is rendered in the navigation bar
     this.props.navigation.setOptions({ title: name });
     
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello Chat!</Text>
-        <Button
-          title='Back to start'
+      // the selected background color is rendered in the chat screen
+      <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: chatBg}}>
+        <Text style={styles.text}>Hello!</Text>
+        <TouchableOpacity
+          style={styles.button}
           // navigates user back to Start screen
-          onPress={() => this.props.navigation.navigate('Start')}
-        />
+          onPress={() => this.props.navigation.navigate('Start')} >
+          <Text style={styles.buttonText}>Back to Start</Text>
+          </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create ({
+  text: {
+    color: 'white',
+    fontSize: 30,
+  },
+  button: {
+    borderWidth: 1,
+    height: 40,
+    margin: 40,
+    padding: 10,
+    borderColor: 'white',
+    
+  },
+  buttonText: {
+    color: 'white',
+  }
+})
+
--- components/Start.js
+++ components/Start.js
@@ -1,72 +1,196 @@
-import React, { Component } from 'react';
+import React from "react";
 
 // imports components from React Native
-import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
 
 // the app's Start component that renders the start page UI
 export default class Start extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: ''}; // the state where user enters their name is empty by default
+    this.state = {
+      name: "", // the state where user enters their name is empty by default
+      chatBg: undefined, // chatBackground is undefined by default
+    };
   }
+
+  // function that allows user to select their chat background color
+  chooseColor = (selectedColor) => this.setState({ chatBg: selectedColor });
+
+  // color array for background
+  colors = {
+    black: "#090c08",
+    darkPurple: "#474056",
+    grayBlue: "#8a95a5",
+    sage: "#b9c6ae",
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <ImageBackground 
-        source={require('./img/ImageBackground.png')}
-        resizeMode='cover'
-        style={styles.image}>
+        <ImageBackground
+          source={require("./img/ImageBackground.png")}
+          resizeMode="cover"
+          style={styles.backgroundImage}
+        >
+          <Text style={styles.appTitle}>Chat</Text>
 
-        <Text
-        style={styles.appTitle}
-        >Chat</Text>
-
-        {/* TextInput component where user will type their name */}
-        <TextInput 
-          style={styles.text}
-          // this function updates empty state to user's name via setState aka passes name to state
-          onChangeText={(name) => this.setState({ name })}
-          // value gets passed to onChangeText
-          value={this.state.name}
-          placeholder='Your Name'
-        />
-        <Button
-          title='Start Chatting'
-          /* when user presses button, the function navigates to the Chat screen and passes
-          the state name from the Start Screen to the Chat screen */
-          onPress={() => this.props.navigation.navigate('Chat', { name: this.state.name })}
-        />
+          <View style={styles.whiteBox}>
+            <View style={styles.inputBox}>
+              {/* imports user icon */}
+              <Image source={require("./img/icon.png")} style={styles.icon} />
+              {/* TextInput component where user will type their name */}
+              <TextInput
+                style={styles.yourNameText}
+                /* this function updates empty state to user's name via 
+                setState a.k.a passes name to state */
+                onChangeText={(name) => this.setState({ name })}
+                // value gets passed to onChangeText
+                value={this.state.name}
+                placeholder="Your Name"
+              />
+            </View>
+            <Text style={styles.chooseBackgroundColorText}>
+              Choose Background Color
+            </Text>
+            <View style={styles.colorChoices}>
+              {/* TouchableOpacity makes views responsive to touches */}
+              <TouchableOpacity
+                style={styles.black}
+                onPress={() => this.chooseColor(this.colors.black)} // user selects color from array
+              />
+              <TouchableOpacity
+                style={styles.darkPurple}
+                onPress={() => this.chooseColor(this.colors.darkPurple)}
+              />
+              <TouchableOpacity
+                style={styles.grayBlue}
+                onPress={() => this.chooseColor(this.colors.grayBlue)}
+              />
+              <TouchableOpacity
+                style={styles.sage}
+                onPress={() => this.chooseColor(this.colors.sage)}
+              />
+            </View>
+            <TouchableOpacity
+              style={styles.button}
+              
+              /* when user presses button, the function navigates to the Chat 
+              screen and passes the state name and background color from the 
+              Start Screen to the Chat screen */
+              onPress={() =>
+                this.props.navigation.navigate("Chat", {
+                  name: this.state.name,
+                  chatBg: this.state.chatBg,
+                })
+              }>
+                <Text style={styles.buttonText}>Start Chatting</Text>
+              </TouchableOpacity>
+          </View>
         </ImageBackground>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
   container: {
-    flex:1, 
-    justifyContent: 'center'
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
   },
   appTitle: {
     fontSize: 45,
-    fontWeight: '600',
-    color: '#FFFFFF',
-    lineHeight: 84,
-    textAlign: 'center',
-
+    fontWeight: "600",
+    color: "#FFFFFF",
+    height: "50%",
+    paddingTop: 70,
   },
-  text: {
-    fontSize: 16,
-    fontWeight: '300',
+  whiteBox: {
+    backgroundColor: "#FFFFFF",
+    height: "44%",
+    width: "88%",
+    justifyContent: "center",
+  },
+  inputBox: {
+    flexDirection: 'row',
+    width: '88%',
+    borderColor: '#757083',
+    borderWidth: 2,
+    padding: 10,
     color: '#757083',
     opacity: 0.5,
-    textAlign: 'center'
+    margin: 20,
   },
-  image: {
-    flex: 1,
-    justifyContent: 'center'
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
   },
-
-})
-
+  yourNameText: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+  },
+  chooseBackgroundColorText: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 1,
+    padding: 20,
+  },
+  colorChoices: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+    padding: 20,
+  },
+  black: {
+    backgroundColor: "#090c08",
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+  },
+  darkPurple: {
+    backgroundColor: "#474056",
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+  },
+  grayBlue: {
+    backgroundColor: "#8a95a5",
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+  },
+  sage: {
+    backgroundColor: "#b9c6ae",
+    borderRadius: 25,
+    width: 50,
+    height: 50,
+  },
+  button: {
+    backgroundColor: "#757083",
+    alignItems: "center",
+    width: "88%",
+    padding: 20,
+    margin: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#FFFFFF",
+  },
+});
