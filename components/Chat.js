6150e98a6c36fb33f38f040534edde97712c4d7b
samskaufel <85969886+samskaufel@users.noreply.github.com>
2022-04-12 20:13:53 -0700

    updated chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -4,16 +4,13 @@ import {
   View,
   Platform,
   KeyboardAvoidingView,
-  StyleSheet,
-  Button,
+  StyleSheet
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
 import "firebase/firestore";
-import * as Permissions from "expo-permissions";
-import * as ImagePicker from "expo-image-picker";
-import * as Location from "expo-location";
 import MapView from 'react-native-maps';
+import CustomActions from './CustomActions';
 const firebase = require("firebase");
 require("firebase/firestore");
 
@@ -66,6 +63,8 @@ export default class Chat extends React.Component {
           _id: data.user._id,
           name: data.user.name,
         },
+        image: data.image || null,
+        location: data.location || null,
       });
     });
     // function renders updates in the app
@@ -173,6 +172,8 @@ export default class Chat extends React.Component {
       text: message.text || "",
       createdAt: message.createdAt,
       user: this.state.user,
+      image: message.image || '',
+      location: message.location || null,
     });
   }
 
@@ -207,6 +208,24 @@ export default class Chat extends React.Component {
     );
   }
 
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+
   renderInputToolbar(props) {
     if (this.state.isConnected == false) {
     } else {
@@ -214,9 +233,27 @@ export default class Chat extends React.Component {
     }
   }
 
-  renderCustomActions = (props) => {
-    return <CustomActions {...props} />;
-  };
+  renderCustomActions = (props) => 
+  <CustomActions {...props} />;
+
+  // Map view for location
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
 
   render() {
     // assigns a variable name to the prop name that is being called from the Start screen
@@ -236,6 +273,7 @@ export default class Chat extends React.Component {
             renderBubble={this.renderBubble.bind(this)}
             renderInputToolbar={this.renderInputToolbar.bind(this)}
             renderActions={this.renderCustomActions}
+            renderCustomView={this.renderCustomView}
             messages={this.state.messages}
             onSend={(messages) => this.onSend(messages)}
             user={{
@@ -246,35 +284,8 @@ export default class Chat extends React.Component {
           {Platform.OS === "android" ? (
             <KeyboardAvoidingView behavior="height" />
           ) : null}
-          <View style={{ flex: 1, justifyContent: "center" }}>
-            <Button
-              title="Pick an image from the library"
-              onPress={this.pickImage}
-            />
-            <Button title="Take a photo" onPress={this.takePhoto} />
-            {this.state.image && (
-              <Image
-                source={{ uri: this.state.image.uri }}
-                style={{ width: 200, height: 200 }}
-              />
-            )}
-
-            <Button title="Get my location" onPress={this.getLocation} />
-
-            {this.state.location && (
-              <MapView
-                style={{ width: 300, height: 200 }}
-                region={{
-                  latitude: this.state.location.coords.latitude,
-                  longitude: this.state.location.coords.longitude,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }}
-              />
-            )}
           </View>
         </View>
-      </View>
     );
   }
 }
